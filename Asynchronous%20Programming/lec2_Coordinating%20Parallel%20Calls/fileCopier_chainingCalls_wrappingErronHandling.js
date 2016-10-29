74eb63b290f2b6162efcd0fc53d1e6fab6dab1a3
Jayam Malviya <malviya.jayam@gmail.com>
2016-10-28 21:52:31 -0700

    adding moore on 2

---
+++ Asynchronous%20Programming/lec2_Coordinating%20Parallel%20Calls/fileCopier_chainingCalls_wrappingErronHandling.js
@@ -0,0 +1,49 @@
+
+var fs  = require('fs');
+var path = require('path');
+var dir = path.join(__dirname,'temp');
+var source = __filename;
+var target = path.join(dir,'target');
+
+fs.mkdir(dir,wrapErrorHandling(makeDirCB));
+
+function makeDirCB (){
+	fs.readFile(source,{encoding:'utf8'},wrapErrorHandling(readFileCB));
+};
+
+function readFileCB (result) {
+	var content = result;
+	fs.writeFile(target,content,wrapErrorHandling(writeFileCB));
+};
+
+function writeFileCB(err, result){
+	console.log('aldone');
+};
+
+/*
+this is how the handleError function ig going to replace the method
+fs.mkdir(dir, function(err,result){
+		if(err){
+				handleError(err);
+		}
+		else{
+			makeDirCB(result);
+		}
+	});
+*/
+function wrapErrorHandling(callBack){
+	return function(err,result){
+		if(err){
+				handleError(err);
+		}
+		else{
+			callBack(result);
+		}
+	};
+};
+
+
+function handleError(err){
+	console.error(err);
+};
+
